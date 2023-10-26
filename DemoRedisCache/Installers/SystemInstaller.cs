03c46f4d2c1623131d9b09814c0704f30ad102ff
nga.vinhpham <101375733+ngaphaminfodation@users.noreply.github.com>
2023-10-26 15:23:26 +0700

    12

--- DemoRedisCache/Installers/SystemInstaller.cs
+++ DemoRedisCache/Installers/SystemInstaller.cs
@@ -1,14 +1,17 @@
-﻿namespace DemoRedisCache.Installers
+﻿using StackExchange.Redis;
+
+namespace DemoRedisCache.Installers
 {
     public class SystemInstaller : IInstaller
     {
         public void InstallServices(IServiceCollection services, IConfiguration configuration)
         {
-            // Add services to the container.
             services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
+
+            services.AddHttpClient();
         }
     }
 }
