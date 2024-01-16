cb3e9e56807cccb53c5d66fb6835a95510a5bb7a
nga.vinhpham <101375733+ngaphaminfodation@users.noreply.github.com>
2024-01-16 11:21:03 +0700

    a

---
+++ DemoRedisCache/Controllers/HyperLogsLogsController.cs
@@ -0,0 +1,29 @@
+﻿using DemoRedisCache.Context;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace DemoRedisCache.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HyperLogsLogsController : ControllerBase
+    {
+        private readonly IDatabase _redisDatabase;
+
+        public HyperLogsLogsController(
+            IRedisConnectionMultiplexer redisConnectionMultiplexer)
+        {
+            _redisDatabase = redisConnectionMultiplexer.GetDatabase();
+        }
+
+        //public async Task<IActionResult> SortedSetPopAscending()
+        //{
+        //    var result = await _redisDatabase.SortedSetPopAsync(
+        //        key: "followers",
+        //        count: 10,
+        //        order: Order.Ascending);
+
+        //    return Ok(result);
+        //}
+    }
+}
---
+++ DemoRedisCache/Controllers/ListController.cs
@@ -0,0 +1,44 @@
+﻿using DemoRedisCache.Context;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace DemoRedisCache.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ListController : ControllerBase
+    {
+        private readonly IDatabase _redisDatabase;
+
+        public ListController(
+            IRedisConnectionMultiplexer redisConnectionMultiplexer)
+        {
+            _redisDatabase = redisConnectionMultiplexer.GetDatabase();
+        }
+
+        [HttpGet("rpush")]
+        public async Task<IActionResult> RightpushAsync()
+        {
+            var key = new RedisKey("temps");
+
+            var values = new string[] { "A", "B", "C" };
+            var redisValues = Array.ConvertAll(values, item => (RedisValue)item);
+      
+            var result = await _redisDatabase.ListRightPushAsync(key, redisValues);
+            return Ok(result);
+        }
+
+        [HttpGet("lpush")]
+        public async Task<IActionResult> LeftpushAsync()
+        {
+            var key = new RedisKey("temps");
+
+            var values = new string[] { "D", "E", "F" };
+            var redisValues = Array.ConvertAll(values, item => (RedisValue)item);
+
+            var result = await _redisDatabase.ListLeftPushAsync(key, redisValues);
+            return Ok(result);
+        }
+    }
+}
+    
